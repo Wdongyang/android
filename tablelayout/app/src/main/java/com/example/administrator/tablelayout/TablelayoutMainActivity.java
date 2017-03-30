1e8983e18fbfbd0391579bde9b8303ffdbb47281
Wdongyang <401318842@qq.com>
2017-03-30 11:33:19 +0800

    tablelayout

---
+++ tablelayout/app/src/main/java/com/example/administrator/tablelayout/TablelayoutMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.tablelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TablelayoutMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tablelayout_main);
+    }
+}
